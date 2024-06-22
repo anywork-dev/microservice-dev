d14cf533eefc08f1d73de2c1e85432bd2eb0463e
Muhammad Fathan <fathnakbar@gmail.com>
2024-06-23 06:31:11 +0700

    initialize other service

---
+++ src/services/email/service.js
@@ -0,0 +1,16 @@
+import crypton from "cryptonite";
+import { randomString } from "helper";
+import { Response } from "net-tools";
+
+class EmailService {
+  static sendVerification({email, code}){
+    
+  }
+
+  static sendVerification({email, code}){
+    
+  }
+}
+
+// Exporting AuthService for external use
+export default AuthService;
